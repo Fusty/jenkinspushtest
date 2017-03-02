6ee4b7b8b3c397bcb2b7a94c7a35199502cfe4c7
Alex Sterling <alex@ethode.com>
2017-03-02 12:32:20 -0500

    Adds test for title

--- tests.js
+++ tests.js
@@ -1,6 +1,10 @@
-casper.test.begin('Homepage Existence', 1, function(test) {
+casper.test.begin('Homepage Existence', 2, function(test) {
     casper.start('http://advancelocal.ethode.com', function() {
         test.assertSelectorHasText('h1', 'Advance Local');
+    });
+
+    casper.then(function(){
+    	test.assertTitle("Advance Local - Dashboard", "Advance Local Dashboard Title Incorrect");
     });
 
     casper.run(function() {
